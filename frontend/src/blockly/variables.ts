0c83b1b6385a73b5071324bd1436b91484a029ab
dgk <dgk@dgk.su>
2016-08-11 14:26:17 +0300

    blockly-test

--- frontend/src/blockly/variables.ts
+++ frontend/src/blockly/variables.ts
@@ -1,3 +1,19 @@
-export const XXX = 1;
+export default {};
 
-console.log(Blockly.Blocks['variables_set']);
+
+Blockly.Blocks['variables_get'].init = function () {
+  this.setHelpUrl(Blockly.Msg.VARIABLES_GET_HELPURL);
+  this.setColour(Blockly.Blocks.variables.HUE);
+  this.appendDummyInput()
+    .appendField(new Blockly.FieldTextInput(), 'TITLE')
+
+    .appendField('(')
+    .appendField(new Blockly.FieldTextInput(
+      Blockly.Msg.VARIABLES_DEFAULT_NAME), 'VAR')
+    .appendField(')')
+
+  this.setOutput(true);
+  this.setEditable(false);
+  this.setTooltip(Blockly.Msg.VARIABLES_GET_TOOLTIP);
+  this.contextMenuMsg_ = Blockly.Msg.VARIABLES_GET_CREATE_SET;
+};
